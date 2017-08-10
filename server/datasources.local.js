f15ef994fff8bb9a31b9706065286e0a36a4223b
Diego Fernández Barrera <bigomby@gmail.com>
2017-08-10 15:36:43 +0200

    :wrench: Use MONGODB_URI on datasources configuration

--- server/datasources.local.js
+++ server/datasources.local.js
@@ -7,6 +7,7 @@ module.exports = {
     port: process.env.DB_PORT || 27017,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.NAME,
+    database: process.env.DB_NAME,
+    url: process.env.MONGODB_URI,
   },
 };
