843e8e332eef823f59a5aafbde5f0ec3a95c08bd
Diego Fernández Barrera <bigomby@gmail.com>
2017-09-08 13:06:24 +0200

    :sparkles: Enable user verification.

--- common/models/user.js
+++ common/models/user.js
@@ -1,5 +1,38 @@
 'use strict';
 
-module.exports = function(User) {
+const path = require('path');
 
+module.exports = User => {
+  if (process.env.NODE_ENV !== 'testing') {
+    User.afterRemote('create', (context, user, next) => {
+      const options = {
+        type: 'email',
+        to: user.email,
+        from: 'noreply@redborder.com',
+        subject: 'Thanks for registering.',
+        template: path.resolve(__dirname, '../../server/views/verify.ejs'),
+        redirect: '/verified',
+        user: user,
+      };
+
+      user.verify(options, (err, response) => {
+        if (err) {
+          User.deleteById(user.id);
+          return next(err);
+        }
+
+        context.res.render(
+          path.resolve(__dirname, '../../server/views/response.ejs'),
+          {
+            title: 'Signed up successfully',
+            content:
+              'Please check your email and click on the verification link ' +
+              'before logging in.',
+            redirectTo: '/',
+            redirectToLinkText: 'Log in',
+          }
+        );
+      });
+    });
+  }
 };
