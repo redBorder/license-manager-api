308409ca4eab9bb66e715d0ec56fba8c2a10f10f
Diego Fernández Barrera <bigomby@gmail.com>
2017-09-12 12:20:16 +0200

    :lipstick: Prefer arrow functions

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ app.set('view engine', 'ejs');
 
 app.start = function() {
   // start the web server
-  return app.listen(function() {
+  return app.listen(() => {
     app.emit('started');
     var baseUrl = app.get('url').replace(/\/$/, '');
     console.log('Web server listening at: %s', baseUrl);
@@ -22,10 +22,9 @@ app.start = function() {
 
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
-boot(app, __dirname, function(err) {
+boot(app, __dirname, (err) => {
   if (err) throw err;
 
   // start the server if `$ node server.js`
-  if (require.main === module)
-    app.start();
+  if (require.main === module) app.start();
 });
