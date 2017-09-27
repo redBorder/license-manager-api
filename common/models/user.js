72a0ae41636ff455676d5235adac78e0ed381951
Diego Fernández Barrera <bigomby@gmail.com>
2017-09-27 11:28:51 +0200

    :sparkles: Disable group methods for user instances.
    This methods will be implemented on the group instance.

--- common/models/user.js
+++ common/models/user.js
@@ -2,7 +2,26 @@
 
 const path = require('path');
 
-module.exports = User => {
+function getApp(model) {
+  return new Promise((resolve, reject) => {
+    model.getApp((err, app) => {
+      if (err) return reject(err);
+      resolve(app);
+    });
+  });
+}
+
+module.exports = async User => {
+  const app = await getApp(User);
+  const Group = app.models.Group;
+
+  User.disableRemoteMethodByName('prototype.__findById__groups');
+  User.disableRemoteMethodByName('prototype.__get__groups');
+  User.disableRemoteMethodByName('prototype.__destroyById__groups');
+  User.disableRemoteMethodByName('prototype.__count__groups');
+  User.disableRemoteMethodByName('prototype.__updateById__groups');
+  User.disableRemoteMethodByName('prototype.__delete__groups');
+
   User.afterRemote('*.__create__groups', async (context, instance) => {
     await instance.members.add(instance.ownerId);
     await instance.save();
