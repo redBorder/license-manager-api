a39582ec5b4e35844c5c9ffcdea5de5a226f8ee6
Diego Fernández Barrera <bigomby@gmail.com>
2017-10-03 13:10:44 +0200

    :fire: Remove dead code

--- common/models/user.js
+++ common/models/user.js
@@ -1,33 +1,6 @@
 'use strict';
 
-const path = require('path');
-
-function getApp(model) {
-  return new Promise((resolve, reject) => {
-    model.getApp((err, app) => {
-      if (err) return reject(err);
-      resolve(app);
-    });
-  });
-}
-
 module.exports = async User => {
-  const app = await getApp(User);
-  const Group = app.models.Group;
-
-  User.disableRemoteMethodByName('prototype.__findById__groups');
-  User.disableRemoteMethodByName('prototype.__get__groups');
-  User.disableRemoteMethodByName('prototype.__destroyById__groups');
-  User.disableRemoteMethodByName('prototype.__count__groups');
-  User.disableRemoteMethodByName('prototype.__updateById__groups');
-  User.disableRemoteMethodByName('prototype.__delete__groups');
-
-  User.afterRemote('*.__create__groups', async (context, instance) => {
-    await instance.members.add(instance.ownerId);
-    await instance.admins.add(instance.ownerId);
-    await instance.save();
-  });
-
   if (process.env.NODE_ENV !== 'testing') {
     User.afterRemote('create', async (context, user) => {
       const options = {
