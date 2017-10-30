a2a86fb46d2d170675dbab2145835871933bad01
Diego Fernández Barrera <bigomby@gmail.com>
2017-10-30 14:44:06 +0100

    :sparkes: Use fake mailer on tests

--- common/models/user.js
+++ common/models/user.js
@@ -1,9 +1,23 @@
 'use strict';
 
+class StubMailer {
+  static send(options, context, cb) {
+    cb(null, null);
+  }
+}
+
 module.exports = function(User) {
-  if (process.env.NODE_ENV !== 'testing') {
-    User.afterRemote('create', async (context, user) => {
-      const options = {
+  User.afterRemote('create', async (context, user) => {
+    let options = null;
+
+    if (process.env.NODE_ENV === 'testing') {
+      options = {
+        type: 'email',
+        from: 'test',
+        mailer: StubMailer,
+      };
+    } else {
+      options = {
         type: 'email',
         protocol: process.env.PROTOCOL || 'http',
         port: process.env.DISPLAY_PORT || 3000,
@@ -12,13 +26,13 @@ module.exports = function(User) {
         from: 'noreply@redborder.com',
         user: user,
       };
+    }
 
-      user.verify(options, (err, response) => {
-        if (err) {
-          User.deleteById(user.id);
-          throw err;
-        }
-      });
+    user.verify(options, (err, response) => {
+      if (err) {
+        User.deleteById(user.id);
+        throw err;
+      }
     });
-  }
+  });
 };
